b12c185d6548b320bfc2b6a895c2507d82c26340
Han <han.lai321@gmail.com>
2019-03-06 22:23:31 -0500

    refactor

--- app/models.js
+++ app/models.js
@@ -1,5 +1,7 @@
 const store = {
-    message:[]
+    message:[],
+    users:[]
+
 };
 
 class Message {
@@ -7,23 +9,65 @@ class Message {
         this.id = data.id;
         this.text = data.text;
         this.isFavorite = false || data.isFavorite;
+        this.userId = data.userId;
         this.update = this.update.bind(this);
     }
     update(data){
         this.text = data.text;
+        this.isFavorite = data.isFavorite || this.isFavorite;
+        this.userId = data.userId || data.userId;
+        return Promise.resolve({
+            id:this.id,
+            text:this.text,
+            isFavorite:this.isFavorite,
+            userId:this.userId,
+        })
     }
 }
 Message.all = ()=>{
         return store.message;
 }
 Message.findById = (id)=>{
-        return store.message.fill(m=>m.id === id);
+    return store.message.find(m=>m.id === id);
 }
 Message.create = (data)=>{
-    const maxId = store.message.map(m=>m.id);
+    const allIds = store.message.map(m=>m.id);
+    const maxId  = allIds.reduce((all,cur)=>Math.max(all,cur),0);
     const newMessage = new Message({isFavorite:false,...data, id: maxId +1});
     
     store.message.push(newMessage);
     return newMessage;
 }
-export {Message};
+
+class User {
+    constructor(data){
+        this.id = data.id;
+        this.name = data.name;
+    }
+    update(data){
+        this.name = data.name;
+        return Promise.resolve({
+            id:this.id,
+            name:this.name
+        });
+    }
+}
+User.all = ()=>{
+    return store.users;
+}
+User.findById = (id)=>{
+    return store.users.find(m=>m.id === id);
+}
+User.create = (data)=>{
+    const allIds = store.users.map(m=>m.id);
+    const maxId  = allIds.reduce((all,cur)=>Math.max(all,cur),0);
+    const newUser = new User({...data, id: maxId +1});
+    
+    store.users.push(newUser);
+    return newUser;
+}
+
+User.create({name:"user1"});
+User.create({name:"user2"})
+Message.create({text:"test message with user 1",userId:1,isFavorite:false});
+export {Message, User};
--- app/schema.js
+++ app/schema.js
@@ -1,16 +1,56 @@
 import { gql } from 'apollo-server-express';
 import { PubSub, withFilter } from 'apollo-server';
-import { Message } from './models';
+import { Message, User } from './models';
+import {delegateToSchema} from 'graphql-tools';
 
+/*************************** Utility function ***************/
+/**
+ * @description Handles the Nodes of the query Node. The query is converted to an AST. Sometimes
+ * multiple queries to the same field can be merged to one to save trips to database. This function
+ * merges multiple query nodes into one (union) version of all requests and make one call to database.
+ * This is especially useful when broadcasting in pugsub
+ * @param {*} returnNodes
+ */
+const mergeReturnNodes = (returnNodes) => {
+    const setOfFields = {};
+    returnNodes.forEach((node) => {
+      const { selectionSet: { selections } } = node;
+      selections.forEach((selection) => {
+        const { name: { value }, selectionSet } = selection;
+        if (setOfFields[value]) {
+          // This field is already in the output, merge it if necessary
+          if (selectionSet) {
+            setOfFields[value] = mergeReturnNodes([setOfFields[value], selection]);
+          } // Otherwise, leave it.
+        } else {
+          // This field is not in the output, put it in
+          setOfFields[value] = selection;
+        }
+      });
+    });
+    // Convert setOfFields to a fieldNode
+    const template = returnNodes[0];
+    const { selectionSet } = template;
+    const newSelectionSet = { ...selectionSet, selections: Object.values(setOfFields) };
+    return { ...template, selectionSet: newSelectionSet };
+  };
+/*************** put it here to just save space ******************/
 const typeDefs = gql`
    type Message {
        id: Int!,
        text: String!,
        isFavorite: Boolean!
+       userId: Int!
+       userByUserId: User!
+   }
+   type User {
+       id: Int!,
+       name: String!
    }
    type Query {
        allMessages: [Message]
        fetchMessage(id: Int!): Message
+       allUsers:[User]
    }
    type Mutation {
        createMessage (
@@ -20,6 +60,7 @@ const typeDefs = gql`
            id: Int!
            text: String!
            isFavorite: Boolean!
+           userId: Int
        ): Message
    },
    type Subscription {
@@ -39,38 +80,59 @@ const resolvers = {
         async allMessages() {
             return await Message.all();
         },
+        async allUsers(){
+            return await User.all();
+        },
         async fetchMessage(_, { id }) {
             return await Message.findById(id);
         },
     },
+    Message:{
+        async userByUserId(parent, args, context, info){
+            // Note there's a problem here, if the user did not ask for userId, then the parent do not
+            // have  userId. Of course for a full stack graphQL server, this problem would have already been solved. 
+            // Let's assume it is solve, after all, this is just to demonstrate subscription
+            return await User.findById(parent.userId);
+        }
+    },
     Mutation: {
-        async createMessage(_, { text }) {
-            const message = await Message.create({ text });
+        async createMessage(_, { text,userId }) {
+            const message = await Message.create({ text, userId });
             await pubsub.publish(MESSAGE_CREATED, { messageCreated: message });
             return message;
         },
-        async updateMessage(_, { id, text, isFavorite}) {
+        async updateMessage(_, { id, text, isFavorite, userId}, context, info) {
             const message = await Message.findById(id);
-            await message.update({text,isFavorite})
+            await message.update({text,isFavorite, userId})
             .then(message=>{
-                const allInfos = Object.values(pubsub.subscriptions).filter(([s, f]) => s === channelName).map(([s, f]) => f());
-                if (allInfos.length < 1) return result;
+                // This block is to handle subscription. the allInfos are all the clients that's subscribed  to this channel
+                // It could be thousands of people. Some of them  may request the nested user, some of them don't. Here I do something tricky
+                // I merge all their request AST, generate a "union" of all the fields, and call a delegate to schema, meaning query with message
+                // This is done by the mergeReturnNodes function
+                const allInfos = Object.values(pubsub.subscriptions).filter(([s, f]) => s === MESSAGE_UPDATED).map(([s, f]) => f());
+                if (allInfos.length < 1) return message;
                 const allNodes = allInfos.map(i => i.fieldNodes[0]);
                 const pubNode = mergeReturnNodes(allNodes);
+                // Then I construct a new info object, which store the AST of the fields.
                 const newInfo = { ...allInfos[0], fieldNodes: [pubNode] };
-                // Do a sql query to get the data
-                newInfo.mergeInfo.delegate(
-                  'query',
-                  `${camelName}ById`,
-                  { id },
-                  context,
-                  newInfo,
-                ).then((pubData) => {
-                  pubsub.publish(channelName, {
-                    [channelName]: pubData,
+                // Do whatever to get the data. It could be constructed query, or just delegate to schema
+                // 
+                console.log(pubNode);
+                delegateToSchema({
+                    schema:info.schema,
+                    operation:'query',
+                    fieldName:'fetchMessage',
+                    args: {id},
+                    context,
+                    info: newInfo 
+                }).then(pubData=>{
+                  // The pubData here should be the union of all fields subscribed by every body. 
+                  // So even if many people subscribe to it, we only need to fetch once. 
+                  // Then publish the data to clients
+                  pubsub.publish(MESSAGE_UPDATED, {
+                    messageUpdated: pubData,
                   });
-                });
-                pubsub.publish(MESSAGE_UPDATED, { messageUpdated: message });
+                })
             });
             return message;
         },
@@ -81,6 +143,7 @@ const resolvers = {
         },
         messageUpdated: {
             subscribe: async (_,args,context,info)=>{
+                // a subscribe function needs to 
                 const iterator = withFilter(
                 () => pubsub.asyncIterator('MESSAGE_UPDATED'),
                     (payload, variables) => {
